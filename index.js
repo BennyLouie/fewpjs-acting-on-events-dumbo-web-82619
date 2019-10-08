e2471a7902ea14b78ad9b70df2970e05f459226d
BennyLouie <bennylouie1412@gmail.com>
2019-10-08 15:03:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 let dodger = document.getElementById("dodger");
 
-dodger.style.backgroundColor = "#FF69B4";
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
 
-dodger.style.left; // "180px"
-dodger.style.bottom; // "0px"
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
 
-dodger.style.bottom = "0px";
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
 
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
